6124ccfc2a76df1cb49549420fe02c8c13042cce
LuXiFa2017 <505184302@qq.com>
2019-05-22 09:53:06 +0800

    2019/05/22 我的界面布局

--- app/src/main/java/mrhao/com/youyuaccount/fragment/HomePageFragment.java
+++ app/src/main/java/mrhao/com/youyuaccount/fragment/HomePageFragment.java
@@ -45,4 +45,5 @@ public class HomePageFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
+
 }
--- app/src/main/java/mrhao/com/youyuaccount/fragment/SettingFragment.java
+++ app/src/main/java/mrhao/com/youyuaccount/fragment/SettingFragment.java
@@ -6,25 +6,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import mrhao.com.youyuaccount.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SettingFragment extends Fragment {
 
 
-    public SettingFragment() {
-        // Required empty public constructor
+    @BindView(R.id.setLayout_them)
+    LinearLayout setLayoutThem;
+    Unbinder unbinder;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        setBaseEvent();
+        return view;
+    }
+
+
+    private void setBaseEvent() {
+
+        //APP主题换肤,support_skin框架
+        setLayoutThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
+
 
 }
